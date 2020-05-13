1519cc4d816ee3055247987031d3a6b588c07eff
Saeed Nazeeruddin <saeedan@uw.edu>
2020-05-13 16:41:07 -0700

    added rudimentary admin page

---
+++ admin.js
@@ -0,0 +1,37 @@
+"use strict";
+(function() {
+
+    function $(id){
+        return document.getElementById(id);
+    }
+
+
+    function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response.text();
+        } else {
+            return Promise.reject(new Error(response.status +
+                                            ": " + response.statusText));
+        }
+    }
+    
+    function ajaxCall(q) {
+        let url = "http://briccwebapp-env.eba-ekqffpav.us-east-1.elasticbeanstalk.com/admin.php?q=" + q; 
+        fetch(url)
+           .then(checkStatus)
+           .then(function(responseText) {
+               console.log(responseText);
+           })
+           .catch(function(error) {
+               //error: do something with error
+               console.log(error);
+           });
+    }
+
+    window.onload = function() {
+        $("queryButton").onclick = function() {
+            console.log("query: " + $("textBox").value);
+            ajaxCall($("textBox").value);
+        }
+    }
+})();
---
+++ admin.php
@@ -0,0 +1,17 @@
+<?php
+include_once "./config/database.php";
+
+$db = connectToDB();
+if (isset($_GET["q"])) {
+    header('Content-Type: application/json');
+    $res = $db->query($_GET["q"]);
+    print(json_encode($res->fetchAll()));
+} else {
+    header("HTTP/1.1 400 Invalid Request");
+    header("Content-type: text/plain");
+    echo("empty param.");
+}
+
+
+
+?>
