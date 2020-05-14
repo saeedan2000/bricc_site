90623a4cd8180ce69223377cf019552b03a60417
Saeed Nazeeruddin <saeedan@uw.edu>
2020-05-13 17:06:26 -0700

    trying to get rid of unnecessary indeces in query result

--- admin.php
+++ admin.php
@@ -5,6 +5,10 @@ $db = connectToDB();
 if (isset($_GET["q"])) {
     header('Content-Type: application/json');
     $res = $db->query($_GET["q"]);
+    $res = $res->fetchAll();
+    for ($i = 0; $i < count($res); $i++) {
+        unset($res[$i]);
+    }
     print(json_encode($res->fetchAll()));
 } else {
     header("HTTP/1.1 400 Invalid Request");
