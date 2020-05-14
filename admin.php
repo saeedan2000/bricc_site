3cdc71ae246ae919fc2b65047cafcf58e5cf2f79
Saeed Nazeeruddin <saeedan@uw.edu>
2020-05-13 17:13:53 -0700

    still trying to remove numeric keys from php response

--- admin.php
+++ admin.php
@@ -6,10 +6,12 @@ if (isset($_GET["q"])) {
     header('Content-Type: application/json');
     $res = $db->query($_GET["q"]);
     $res = $res->fetchAll();
-    for ($i = 0; $i < count($res); $i++) {
-        unset($res[$i]);
-    }
-    print(json_encode($res->fetchAll()));
+    $output = [];
+    foreach ($res as $k => $v)
+        $output[] = (is_string($k) ? ('"' . $k . '":') : '') . json_encode($v);
+
+    echo '{' . implode(',', $output) . '}' . PHP_EOL;
+    #print(json_encode());
 } else {
     header("HTTP/1.1 400 Invalid Request");
     header("Content-type: text/plain");
